030782237c64dd485e34acdbf8d766976e8a9a0f
afeiship <1290657123@qq.com>
2019-12-05 22:00:11 +0800

    add: rubym

---
+++ README.md
@@ -0,0 +1,7 @@
+# ushell-module-rubymime
+> Rubymime for ubuntu.
+
+## installation
+```shell
+npm i -S afeiship/ushell-module-rubymime
+```
---
+++ index.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+## path:
+ROOT_PATH=$(dirname $BASH_SOURCE);
+
+RUBYMIME_HOME="$HOME/soft/"
+
+alias rbm="$RUBYMIME_HOME/bin/rubymime.sh";
+
+unset ROOT_PATH;
